98bbaf36bc7ea0ebc1f956e4499d74ce081bc490
maximegregoire <maximegregoireeng@gmail.com>
2014-11-15 02:33:09 -0500

    Added base project

---
+++ facerecognition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace facerecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = GetFilePath(args[0]);
+        }
+
+        static string GetFilePath(string arg)
+        {
+            string folderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string completeFilePath = Path.Combine(folderPath, arg);
+            if (!File.Exists(completeFilePath))
+            {
+                Console.Out.Write("The specified file cannot be found in the current folder");
+                throw new FileNotFoundException("Could not find the file", arg);
+            }
+
+            return completeFilePath;
+        }
+    }
+}
