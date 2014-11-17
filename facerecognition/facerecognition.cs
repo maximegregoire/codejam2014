75bad6693a75afec574260e63af3be22d4566fa4
maximegregoire <maximegregoireeng@gmail.com>
2014-11-16 19:00:15 -0500

    Commented Person.cs

--- facerecognition/facerecognition.cs
+++ facerecognition/facerecognition.cs
@@ -15,7 +15,6 @@ using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Windows.Forms;
 
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -24,8 +23,6 @@ using Emgu.CV.CvEnum;
 using Emgu.CV.Features2D;
 using Emgu.CV.Util;
 using Emgu.CV.Flann;
-
-using ModelAnalysis;
 
 namespace facerecognition
 {
@@ -124,63 +121,9 @@ namespace facerecognition
                     return;
                 }
 
-                //Console.Out.WriteLine(IdentifyFaceWithDataset(args[0], trainingDataset));
-                IdentifyFaceWithDataset(args[0], trainingDataset);
+                Console.Out.WriteLine(IdentifyFaceWithDataset(args[0], trainingDataset));
             }
-
-           // Tests.testAverageKeypointFastOnRealPhotos();
-            //Tests.testAverageKeypointFast();
         }
-
-        /*
-        public static int IdentifyAverageCommonKeypointFast(string filePath, bool shouldExtractDatabase = true)
-        {
-            string folderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            //var dbFiles = Directory.GetFiles(Path.Combine(folderPath, Program.trainingDataset), "*.gif");
-            var dictionary = new Dictionary<int, Person>();
-
-            if (shouldExtractDatabase)
-            {
-           //     database = ExtractDatabase(folderPath);
-            }
-
-            VectorOfKeyPoint unknownKeyPoints;
-            Matrix<Byte> unknownDescriptors;
-
-            using (FastDetector fastCPU = new FastDetector(FAST_TRESHOLD_PROCESSING, NON_MAXIMAL_SUPRESSION))
-            using (var descriptor = new BriefDescriptorExtractor())
-            using (var unknownImage = new Image<Gray, byte>(filePath))
-            {
-                unknownKeyPoints = fastCPU.DetectKeyPointsRaw(unknownImage, null);
-                unknownDescriptors = descriptor.ComputeDescriptorsRaw(unknownImage, null, unknownKeyPoints);
-            }
-            VectorOfKeyPoint dbKeyPoints;
-            foreach (var subject in ModelAnalysisDataSerializer.GetModelAnalyses(Path.Combine(folderPath, "Database")))
-            {
-                foreach(var photo in subject.photoAnalyses)
-                {
-                    dbKeyPoints = new VectorOfKeyPoint();
-                    dbKeyPoints.Push(photo.keypoints);
-                    int computedKeypoints = facerecognition.GetCommonKeypointsFast(unknownDescriptors, unknownKeyPoints, photo.descriptors, dbKeyPoints);
-
-                    //TODO: handle possible error from parsing
-                    if (!dictionary.ContainsKey(subject.subjectId))
-                    {
-                        dictionary.Add(subject.subjectId, new Person(subject.subjectId));
-                    }
-
-                    var person = new Person(0);
-                    if (!dictionary.TryGetValue(subject.subjectId, out person))
-                    {
-                        throw new InvalidOperationException("something went wrong with the dictionary");
-                    }
-
-                    person.AddComparison(computedKeypoints);
-                }
-            }
-
-            return dictionary.Aggregate((l, r) => l.Value.AverageCommonKeypoints > r.Value.AverageCommonKeypoints ? l : r).Key;
-        }*/
 
         /// <summary>
         /// Checks that a given file is an image
@@ -199,7 +142,7 @@ namespace facerecognition
         /// </summary>
         /// <param name="picturePath">The path of the picture</param>
         /// <returns>The cropped and scalled image</returns>
-        public static Image<Gray, byte> CropAndScalePicture(string picturePath)
+        private static Image<Gray, byte> CropAndScalePicture(string picturePath)
         {
             Bitmap myBitmap = new Bitmap(picturePath);
             Rectangle cloneRect = new Rectangle(CROP_WIDTH, CROP_HEIGHT_BOTTOM, myBitmap.Width - (2 * CROP_WIDTH), myBitmap.Height - CROP_HEIGHT_BOTTOM - CROP_HEIGHT_TOP);
@@ -248,8 +191,6 @@ namespace facerecognition
                 fastCPU = new FastDetector(FAST_TRESHOLD_ACQUIRING, NON_MAXIMAL_SUPRESSION);
                 foreach (var dbFile in dbFiles)
                 {
-
-                    // TODO ::   ERASE THIS WHEN DONE TESTINGGGGGGGGGGGGGGGGGGGGGGGGGGGG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                     if (dbFile.Equals(filePath))
                     {
                         continue;
@@ -269,7 +210,7 @@ namespace facerecognition
                     int computedKeypoints = facerecognition.GetCommonKeypointsFast(unknownDescriptors, unknownKeyPoints, dbDescriptors, dbKeyPoints);
 
                     //TODO: handle possible error from parsing
-                    int subjectId = Convert.ToInt32(Path.GetFileName(dbFile.Split('_')[0]));
+                    int subjectId = Convert.ToInt32(Path.GetFileName(dbFile).Split('_')[0]);
 
                     if (!dictionary.ContainsKey(subjectId))
                     {
@@ -288,107 +229,7 @@ namespace facerecognition
 
                 return dictionary.Aggregate((l, r) => l.Value.AverageCommonKeypoints > r.Value.AverageCommonKeypoints ? l : r).Key;
             }
-        }
-
-
-
-
-        /// <summary>
-        /// Identifies the face on the file given, using the dataset at the given path
-        /// </summary>
-        /// <param name="filePath"></param>
-        /// <param name="datasetPath"></param>
-        /// <returns></returns>
-        /*public static int IdentifyFaceWithDatasetSurf(string filePath, string datasetPath)
-        {
-            var dbFiles = Directory.GetFiles(datasetPath, "*.gif");
-            var dictionary = new Dictionary<int, Person>();
-            VectorOfKeyPoint unknownKeyPoints = new VectorOfKeyPoint();
-            Matrix<float> unknownDescriptors;
-            SURFDetector surfCPU = new SURFDetector(HESSIAN_TRESH, NON_MAXIMAL_SUPRESSION);
-            using (var unknownImage = new Image<Gray, byte>(filePath))
-            {
-                var keypointArray = surfCPU.DetectKeyPointsRaw(unknownImage, null).ToArray().Where(k => k.Response > POINT_RESPONSE_LOWER_SURF).ToArray();
-                unknownKeyPoints.Push(keypointArray);
-                unknownDescriptors = surfCPU.ComputeDescriptorsRaw(unknownImage, null, unknownKeyPoints);
-
-
-                IList<IndecesMapping> imap;
-
-                // compute descriptors for each image
-                var dbDescsList = ComputeMultipleDescriptors(dbFiles, out imap);
-
-                // concatenate all DB images descriptors into single Matrix
-                Matrix<float> dbDescs = ConcatDescriptors(dbDescsList);
-
-                // compute descriptors for the query image
-                Matrix<float> queryDescriptors = ComputeSingleDescriptors(filePath);
-
-                FindMatches(dbDescs, queryDescriptors, ref imap);
-
-                var max = imap.OrderByDescending(i => i.IndexEnd).FirstOrDefault();
-                return 3;
-
-
-                /*foreach (var dbFile in dbFiles)
-                {
-
-                    // TODO ::   ERASE THIS WHEN DONE TESTINGGGGGGGGGGGGGGGGGGGGGGGGGGGG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                    if (dbFile.Equals(filePath))
-                    {
-                        continue;
-                    }
-
-                    var dbImage = new Image<Gray, byte>(dbFile);
-
-
-                    VectorOfKeyPoint dbKeyPoints = new VectorOfKeyPoint();
-
-                    var dbKeypointArray = surfCPU.DetectKeyPointsRaw(dbImage, null).ToArray().Where(k => k.Response > POINT_RESPONSE_LOWER_SURF).ToArray();
-                    dbKeyPoints.Push(dbKeypointArray);
-
-                    Matrix<float> dbDescriptors = surfCPU.ComputeDescriptorsRaw(dbImage, null, dbKeyPoints);
-
-
-                    IList<IndecesMapping> imap = new List<IndecesMapping>();
-
-                    GetMatches(unknownDescriptors, unknownKeyPoints, dbDescriptors, dbKeyPoints, ref imap);
-                    int computedKeypoints = imap.Count;//facerecognition.GetCommonKeypointsSurf(unknownDescriptors, unknownKeyPoints, dbDescriptors, dbKeyPoints);
-
-                    //TODO: handle possible error from parsing
-                    int subjectId = Convert.ToInt32(Path.GetFileName(dbFile.Split('_')[0]));
-
-                    if (!dictionary.ContainsKey(subjectId))
-                    {
-                        dictionary.Add(subjectId, new Person(subjectId));
-                    }
-
-                    var person = new Person(0);
-                    if (!dictionary.TryGetValue(subjectId, out person))
-                    {
-                        throw new InvalidOperationException("something went wrong with the dictionary");
-                    }
-
-                    person.AddComparison(computedKeypoints);
-
-                }
-
-                return dictionary.Aggregate((l, r) => l.Value.AverageCommonKeypoints > r.Value.AverageCommonKeypoints ? l : r).Key;
-            }
-        }*/
-
-        /*public static Dictionary<int, ICollection<PhotoAnalysisData>> ExtractDatabase(string currentFolder)
-        {
-            string databaseFolder = Path.Combine(currentFolder, "Database");
-            var dictionary = new Dictionary<int, ICollection<PhotoAnalysisData>>();
-            //Deserialize objects
-            foreach (var modelData in ModelAnalysisDataSerializer.GetModelAnalyses(databaseFolder))
-            {
-                dictionary.Add(modelData.subjectId, modelData.photoAnalyses);
-            }
-
-            return dictionary;
-        }*/
+        }    
 
         /// <summary>
         /// Gets the amount of common key points between two sets of descriptors and key points, using a fast generator
@@ -433,64 +274,5 @@ namespace facerecognition
 
             return numOfKeypoints;
         }
-
-        /*
-        public static int GetCommonKeypointsSurf(Matrix<float> unknownDescriptors, VectorOfKeyPoint unknownImageKeyPoints, Matrix<float> dbDescriptors, VectorOfKeyPoint dbImageKeyPoints, ref IList<IndecesMapping> imap)
-        {
-            SURFDetector fastCPU = new SURFDetector(HESSIAN_TRESH, NON_MAXIMAL_SUPRESSION);
-            Matrix<int> indices = new Matrix<int>(dbDescriptors.Rows, 2);
-            var dists = new Matrix<float>(dbDescriptors.Rows, 2);
-
-            BriefDescriptorExtractor descriptor = new BriefDescriptorExtractor();
-
-            Matrix<byte> mask;
-
-            // extract features from the observed image
-
-            var flannIndex = new Index(unknownDescriptors, 4);
-            flannIndex.KnnSearch(dbDescriptors, indices, dists, 2, 24);
-            for (int i = 0; i < indices.Rows; i++)
-            {
-                // filter out all inadequate pairs based on distance between pairs
-                if (dists.Data[i, 0] < (0.6 * dists.Data[i, 1]))
-                {
-                    // find image from the db to which current descriptor range belongs and increment similarity value.
-                    // in the actual implementation this should be done differently as it's not very efficient for large image collections.
-                    foreach (var img in imap)
-                    {
-                        if (img.IndexStart <= i && img.IndexEnd >= i)
-                        {
-                            img.Similarity++;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            BruteForceMatcher<float> matcher = new BruteForceMatcher<float>(BFM_OPTION);
-            matcher.Add(unknownDescriptors);
-
-            if (dbDescriptors == null)
-            {
-                return 0;
-            }
-
-            indices = new Matrix<int>(dbDescriptors.Rows, KNN_MATCH_CONSTANT);
-            using (Matrix<float> dist = new Matrix<float>(dbDescriptors.Rows, KNN_MATCH_CONSTANT))
-            {
-                matcher.KnnMatch(dbDescriptors, indices, dist, KNN_MATCH_CONSTANT, null);
-                mask = new Matrix<byte>(dist.Rows, 1);
-                mask.SetValue(255);
-                Features2DToolbox.VoteForUniqueness(dist, UNIQUENESS_THRESHOLD, mask);
-            }
-
-            int numOfKeypoints = CvInvoke.cvCountNonZero(mask);
-            if (numOfKeypoints >= 4)
-            {
-                return Features2DToolbox.VoteForSizeAndOrientation(unknownImageKeyPoints, dbImageKeyPoints, indices, mask, SCALE_INCREMENT, ROTATION_BINS);
-            }
-
-            return numOfKeypoints;
-        }*/
     }
 }
